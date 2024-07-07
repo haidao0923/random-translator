2dd5d3b97ea9e9286a8a7b2785968c22f40a6805
hdao35 <hdao35@gatech.edu>
2024-07-07 16:49:31 -0400

    temp solution to scraping lyrics with Genius API + customized scraping

--- random_translator.py
+++ random_translator.py
@@ -1,10 +1,34 @@
 import useful_functions
 import song_scraper
+import requests
+from bs4 import BeautifulSoup, NavigableString
+import re
 
-song_titles = song_scraper.scrape_billboard_hot_song_titles()
+import os
+from dotenv import load_dotenv
 
-for i in range(len(song_titles)):
-    print(i, song_titles[i])
+
+load_dotenv()
+
+GENIUS_API_ACCESS_TOKEN = os.getenv("GENIUS_API_ACCESS_TOKEN")
+
+#song_titles = song_scraper.scrape_billboard_hot_song_titles()
+
+#for i in range(len(song_titles)):
+#    print(i, song_titles[i])
+
+search_response = requests.get('https://api.genius.com/search?q=A%20Bar%20Song%20(Tipsy)', headers={'Authorization': f'Bearer {GENIUS_API_ACCESS_TOKEN}'})
+song_api_path = "https://api.genius.com" + search_response.json()["response"]["hits"][0]["result"]["api_path"]
+print("Song API: ", song_api_path)
+song_response = requests.get(song_api_path, headers={'Authorization': f'Bearer {GENIUS_API_ACCESS_TOKEN}'})
+lyric_page_path = song_response.json()["response"]["song"]["url"]
+print("Lyric Page", lyric_page_path)
+
+lyric_response = requests.get(lyric_page_path)
+soup = BeautifulSoup(lyric_response.content, 'html.parser')
+lyric_container = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-1")
+text = lyric_container.get_text(strip=True, separator="\n")
+print(text)
 
 file_path = 'text.txt'
 
